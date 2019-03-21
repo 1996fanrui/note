443cb0afb9a08d74e9fd876de007755849e66513
fanrui <fanrui@weli.cn>
2019-03-21 23:04:40 +0800

    Enhancement: 增加了链表的练习题，单链表反转，检测是否有环，查找链表中间节点，合并两个有序链表，删除倒数第k个节点

---
+++ datastruct/src/main/java/fanrui/study/linkedlist/FindMiddleNode.java
@@ -0,0 +1,27 @@
+package fanrui.study.linkedlist;
+
+public class FindMiddleNode {
+
+    /**
+     *  求链表的中间结点
+     * @param list
+     * @return
+     * @desc 两个指针a,b初始时都指向头指针，a指针每次往后移动一个元素，b指针每次往后移动两个元素，
+     *          当b指针指向链表尾时，a指针就指向了链表的中间节点
+     */
+    public static Node findMiddleNode(Node list) {
+        if (list == null) {
+            return null;
+        }
+
+        Node fast = list;
+        Node slow = list;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
+}
--- datastruct/src/test/java/fanrui/study/sort/SortPerformanceTest.java
+++ datastruct/src/test/java/fanrui/study/sort/SortPerformanceTest.java
@@ -4,7 +4,6 @@ import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 /**
  * 排序测试
